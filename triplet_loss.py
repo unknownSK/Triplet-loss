1c51f48461033a10eabc2ae5472b1c0c1cac0892
Suryakant Tibrewal <37638352+unknownSK@users.noreply.github.com>
2018-12-24 13:46:29 +0530

    Create triplet_loss.py

---
+++ triplet_loss.py
@@ -0,0 +1,151 @@
+from __future__ import print_function
+import tensorflow as tf
+from keras.datasets import mnist
+import numpy as np
+import keras.backend as K
+
+# Define loss
+def loss(output,labels):
+    triplet = tf.contrib.losses.metric_learning.triplet_semihard_loss(labels,output,margin=margin)
+    loss=tf.reduce_mean(triplet,name='triplet')
+    return loss
+
+def traning(cost,global_step):
+    tf.summary.scalar("cost",cost)
+    learning_rate=tf.train.exponential_decay(0.01,global_step,100,0.96,staircase=True)
+    optimizer=tf.train.AdamOptimizer(learning_rate)
+    train_op=optimizer.minimize(cost,global_step=global_step)
+    return train_op
+
+# Architecture
+def inference(x):
+    phase_train = tf.constant(True)
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+
+    conv1 = tf.layers.conv2d(inputs=x, filters=32,  kernel_size=[7, 7], padding="same", activation=tf.nn.relu)
+    norm1 = tf.layers.batch_normalization(conv1)
+    pool1 = tf.layers.max_pooling2d(inputs=norm1, pool_size=[2, 2], strides=2)
+
+    conv2a = tf.layers.conv2d(inputs=pool1, filters=32, kernel_size=[1, 1], padding="same", activation=tf.nn.relu)
+    conv2 = tf.layers.conv2d(inputs=conv2a, filters=64, kernel_size=[3, 3], padding="same", activation=tf.nn.relu)
+    norm2 = tf.layers.batch_normalization(conv2)
+    pool2 = tf.layers.max_pooling2d(inputs=norm2, pool_size=[2, 2], strides=2)
+
+    conv3a = tf.layers.conv2d(inputs=pool2, filters=32, kernel_size=[1, 1], padding="same", activation=tf.nn.relu)
+    conv3 = tf.layers.conv2d(inputs=conv3a, filters=64, kernel_size=[3, 3], padding="same", activation=tf.nn.relu)
+
+    conv4a = tf.layers.conv2d(inputs=conv3, filters=32, kernel_size=[1, 1], padding="same", activation=tf.nn.relu)
+    conv4 = tf.layers.conv2d(inputs=conv4a, filters=64, kernel_size=[3, 3], padding="same", activation=tf.nn.relu)
+
+    flat = tf.reshape(conv4, [-1, 7 * 7 * 64])
+
+    fc_1 = tf.layers.dense(inputs=flat, units=128, activation=tf.nn.relu)
+
+    embed = tf.layers.dense(inputs=fc_1, units=128)
+
+    output = tf.nn.l2_normalize(embed, dim=1, epsilon=1e-12, name=None)
+
+    return output
+    
+if __name__=='__main__':
+    # Paramaters
+    training_epochs=1
+    display_step=1
+    batch_size=512
+    margin=1.0
+    img_rows,img_cols=28,28
+    (x_train,y_train),(x_test,y_test)=mnist.load_data()
+    
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+        
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+    print('y_train shape:', y_train.shape)
+    
+    x_train_real = []
+    for i in range(int(60000)):
+        img = x_train[i].reshape((28, 28))
+        x_train_real.append(img.reshape((28, 28, 1)))
+
+    x_train_real = np.array(x_train_real)
+    
+    x=tf.placeholder(tf.float32,[None,28,28,1],name='placeholder_x')
+    y=tf.placeholder(tf.float32,[None],name='placeholder_y')
+    
+    output=inference(x)
+    
+    tf.identity(output,name="inference")
+    
+    cost=loss(output,y)
+    
+    global_step=tf.Variable(0,name='global_step',trainable=False)
+    
+    train_op=traning(cost,global_step)
+    
+    saver=tf.train.Saver()
+    
+    features_placeholder=tf.placeholder(x_train_real.dtype,x_train_real.shape)
+    labels_placeholder=tf.placeholder(y_train.dtype,y_train.shape)
+    
+    traning_dataset=tf.data.Dataset.from_tensor_slices((features_placeholder,labels_placeholder))
+    batched_dataset=traning_dataset.batch(batch_size)
+    
+    traning_init_op=batched_dataset.make_initializable_iterator()
+    next_element = traning_init_op.get_next()
+    
+    
+    init_op=tf.global_variables_initializer()
+    
+    with tf.device('/cpu:0'):
+        with tf.Session() as sess:
+            sess.run(init_op)
+            # Training Cycle
+            for epoch in range(training_epochs):
+                avg_cost=0
+                total_batch_train=int(x_train.shape[0]/batch_size)
+                sess.run(traning_init_op.initializer,feed_dict={features_placeholder:x_train_real,labels_placeholder:y_train})
+                
+                #Loop over all the branches
+                for i in range(total_batch_train):
+                    batch_x,batch_y=sess.run(next_element)
+                    sess.run(train_op,feed_dict={x:batch_x,y:batch_y})
+                    
+                    avg_cost=sess.run(cost,feed_dict={x:batch_x,y:batch_y})
+                    
+                    if not i % 5:
+                        print ('Epoch #:', epoch ,' global_step', sess.run(global_step),' Batch #: ',i,' loss: ',avg_cost)
+                              
+            saver.save(sess,"model_logs/model-checkpoint",global_step=global_step, write_meta_graph=True)
+            
+            
+            
+            
+        
+            predict_op=tf.argmax(output,1)
+            correct_pred=tf.equal(predict_op,tf.arg_max(labels_placeholder,1))
+            
+            accuracy = tf.reduce_mean(tf.cast(correct_pred, "float"))
+            
+            train_accuracy = accuracy.eval({x: x_train, y: y_train})
+            test_accuracy = accuracy.eval({y: y_test, y: y_test})
+            print ("Train Accuracy:", accuracy.eval({x: x_train, y: y_train}))
+            print ("Test Accuracy:", accuracy.eval({y: y_test, Y: y_test}))
+             
+             
+        print("Optimization Finished!")
+
+    
+    
+    
